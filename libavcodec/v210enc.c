9730b97e084a577bc7e1a4d4a94d75db90d9c4bb
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2025-01-17 15:50:05 +0100

    DeckLink HDR metadata output support (from the ffmpeg-devel mailing list)

--- libavcodec/v210enc.c
+++ libavcodec/v210enc.c
@@ -123,6 +123,15 @@ static int encode_frame(AVCodecContext *avctx, AVPacket *pkt,
     AVFrameSideData *side_data;
     int ret;
     uint8_t *dst;
+    static const struct {
+        enum AVPacketSideDataType packet;
+        enum AVFrameSideDataType frame;
+    } sd[] = {
+        { AV_PKT_DATA_A53_CC,                     AV_FRAME_DATA_A53_CC },
+        { AV_PKT_DATA_AFD,                        AV_FRAME_DATA_AFD },
+        { AV_PKT_DATA_MASTERING_DISPLAY_METADATA, AV_FRAME_DATA_MASTERING_DISPLAY_METADATA },
+        { AV_PKT_DATA_CONTENT_LIGHT_LEVEL,        AV_FRAME_DATA_CONTENT_LIGHT_LEVEL },
+    };
 
     ret = ff_alloc_packet2(avctx, pkt, avctx->height * stride, avctx->height * stride);
     if (ret < 0) {
@@ -136,20 +145,14 @@ static int encode_frame(AVCodecContext *avctx, AVPacket *pkt,
     else if(pic->format == AV_PIX_FMT_YUV422P)
         v210_enc_8(avctx, dst, pic);
 
-    side_data = av_frame_get_side_data(pic, AV_FRAME_DATA_A53_CC);
-    if (side_data && side_data->size) {
-        uint8_t *buf = av_packet_new_side_data(pkt, AV_PKT_DATA_A53_CC, side_data->size);
-        if (!buf)
-            return AVERROR(ENOMEM);
-        memcpy(buf, side_data->data, side_data->size);
-    }
-
-    side_data = av_frame_get_side_data(pic, AV_FRAME_DATA_AFD);
-    if (side_data && side_data->size) {
-        uint8_t *buf = av_packet_new_side_data(pkt, AV_PKT_DATA_AFD, side_data->size);
-        if (!buf)
-            return AVERROR(ENOMEM);
-        memcpy(buf, side_data->data, side_data->size);
+    for (int i = 0; i < FF_ARRAY_ELEMS(sd); i++) {
+        side_data = av_frame_get_side_data(pic, sd[i].frame);
+        if (side_data && side_data->size) {
+            uint8_t *buf = av_packet_new_side_data(pkt, sd[i].packet, side_data->size);
+            if (!buf)
+                return AVERROR(ENOMEM);
+            memcpy(buf, side_data->data, side_data->size);
+        }
     }
 
     pkt->flags |= AV_PKT_FLAG_KEY;
